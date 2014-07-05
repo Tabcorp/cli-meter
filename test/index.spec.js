7a2e82acf1f071e6fd848d6f9c4b9bf23f7014e2
rprieto <choicesmade@gmail.com>
2014-07-05 17:39:49 +1000

    Unit tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,30 +1,41 @@
+require('should');
 var Meter = require('../lib/index');
 
 describe('Meter', function() {
   
-  it('Can display several meters at once', function(done) {
+  it('has a default length of 20', function() {
+    var m = new Meter();
+    m.toString().should.eql('[                    ]');
+  });
+
+  it('can specify the length', function() {
+    var m = new Meter({length: 10});
+    m.toString().should.eql('[          ]');
+  });
+
+  it('can display several meters at once', function(done) {
 
     this.slow(5000);
     this.timeout(5000);
 
     console.log('\n\n');
 
-    var p1 = new Meter({total: 20, value: 5});
-    var p2 = new Meter({total: 20, value: 10});
-    var p3 = new Meter({total: 20, value: 15});
+    var m1 = new Meter({total: 20, value: 5});
+    var m2 = new Meter({total: 20, value: 10});
+    var m3 = new Meter({total: 20, value: 15});
     
     var id = setInterval(function() {
     
-      p1.step(1 - Math.random() * 2);
-      p2.step(1 - Math.random() * 2);
-      p3.step(1 - Math.random() * 2);
+      m1.step(1 - Math.random() * 2);
+      m2.step(1 - Math.random() * 2);
+      m3.step(1 - Math.random() * 2);
     
       process.stdout.write('\n');
       process.stdout.write('\n');
       process.stdout.write('\n');
-      process.stdout.write('   Meter 1 ' + p1 + '  \n');
-      process.stdout.write('   Meter 2 ' + p2 + '  \n');
-      process.stdout.write('   Meter 3 ' + p3 + '  \n');
+      process.stdout.write('   Meter 1 ' + m1 + '  \n');
+      process.stdout.write('   Meter 2 ' + m2 + '  \n');
+      process.stdout.write('   Meter 3 ' + m3 + '  \n');
       process.stdout.write('\n');
       process.stdout.write('\n');
       process.stdout.write('\n');
@@ -38,7 +49,7 @@ describe('Meter', function() {
       clearInterval(id);
       console.log('\n\n\n\n\n\n\n\n\n');
       done();
-    }, 2000);
+    }, 1500);
   });
 
 });
